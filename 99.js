55453e6f2e3d7bc048aea6917099061c4e642b5a
Scott Roberson <srkazan23@gmail.com>
2018-03-03 17:03:45 -0600

    99 Bottles of Beer javascript while loop

---
+++ 99.js
@@ -0,0 +1,5 @@
+var bottles=99;
+while(bottles >0){
+	console.log(bottles + " Bottles of Beer of beer on the wall! "+ bottles + " bottles of beer. Take one down, pass it around " + (bottles-1) + " bottles of beer on the wall ");
+	bottles--;
+}
